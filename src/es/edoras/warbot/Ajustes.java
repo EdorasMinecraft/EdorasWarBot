c9597d67be0147717b707e36322df81f4497df64
Kikisito <35898087+Kikisito@users.noreply.github.com>
2019-07-16 13:16:54 +0200

    Delete Ajustes.java

--- src/es/edoras/warbot/Ajustes.java
+++
@@ -1,8 +0,0 @@
-package es.edoras.warbot;
-
-public interface Ajustes {
-	
-	public String[] nombres = {"Persona1", "Persona2", "Persona3", "Persona4", "Persona5", "Persona6", "Persona7", "Persona8"};
-	
-	public boolean SuicidiosActivados = true;
-}
